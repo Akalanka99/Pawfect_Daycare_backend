59336487c66485b8641cbd74d5e846427525b526
Akalanka Savindu <147297172+Akalanka99@users.noreply.github.com>
2024-09-30 23:04:31 +0530

    updated

--- src/main/java/com/pawfectdaycare/pawfectdaycare/controller/AuthController.java
+++ src/main/java/com/pawfectdaycare/pawfectdaycare/controller/AuthController.java
@@ -25,7 +25,7 @@ import java.util.Map;
 @CrossOrigin
 @RequestMapping("/api/auth")
 public class AuthController {
-    //erwewrrr
+
 
     @Autowired
     private AuthenticationManager authenticationManager;
