31915ee5fb78b51ae32e99f31cb29a855a8d0792
Akalanka Savindu <147297172+Akalanka99@users.noreply.github.com>
2025-03-19 22:57:51 +0530

    update

--- src/main/java/com/pawfectdaycare/pawfectdaycare/entity/User.java
+++ src/main/java/com/pawfectdaycare/pawfectdaycare/entity/User.java
@@ -13,13 +13,10 @@ public class User {
     private String uid; // Firebase UID
     private String name; // Display name
 
-    private String uid; // Firebase UID
-    private String name; // Display name
+
     private String email;
     private String password;
 
-    @Enumerated(EnumType.STRING)
-    private Role role;
 
     // Constructors
     @Enumerated(EnumType.STRING)
@@ -31,21 +28,9 @@ public class User {
     // Constructor with uid
     public User(String uid, String name, String email, String password, Role role) {
         this.uid = uid;
-    // Constructor with uid
-    public User(String uid, String name, String email, String password, Role role) {
-        this.uid = uid;
         this.name = name;
         this.email = email;
         this.password = password;
-        this.role = role;
-    }
-
-    // Constructor without uid (for manual user registration)
-    public User(String name, String email, String password, Role role) {
-        this.name = name;
-        this.email = email;
-        this.password = password;
-        this.role = role;
         this.role = role;
     }
 
@@ -74,14 +59,6 @@ public class User {
         this.uid = uid;
     }
 
-    public String getUid() {
-        return uid;
-    }
-
-    public void setUid(String uid) {
-        this.uid = uid;
-    }
-
     public String getName() {
         return name;
     }
@@ -99,11 +76,9 @@ public class User {
     }
 
     public String getPassword() {  // Add getter for password
-    public String getPassword() {  // Add getter for password
         return password;
     }
 
-    public void setPassword(String password) {  // Add setter for password
     public void setPassword(String password) {  // Add setter for password
         this.password = password;
     }
@@ -112,7 +87,4 @@ public class User {
 
     public void setRole(Role role) { this.role = role; }
 
-    public Role getRole() { return role; }
-
-    public void setRole(Role role) { this.role = role; }
 }
